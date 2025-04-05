f9129c7182fce55fe3e1f92974b06ba5453b6b2b
raisa-farzana <raisafarzana5502@gmail.com>
2025-04-05 13:04:29 +0600

    updated html js files

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const addTask = () => {
     const taskInput = document.getElementById('taskInput');
-    const taskInputValue = taskInput.ariaValueMax.trim();
+    const taskInputValue = taskInput.value.trim();
 
     if(taskInputValue !== ""){
         const taskListUl = document.getElementById('taskList');
         const li = document.createElement('li');
-        li.innerHTML = '<input type="checkbox">' + taskInputValue + '<button> delete </button>'
+        li.innerHTML = '<input type="checkbox"> ' + taskInputValue + ' <button onclick="deleteTask(this)"> delete </button>';
+        taskListUl.appendChild(li);
+
+        taskInput.value = "";
+
     }
     else{
         alert("Please Add a Task")
     }
+}
+
+const deleteTask = (btn) => {
+    const li = btn.parentNode;
+    li.parentNode.removeChild(li);
+ 
 }
